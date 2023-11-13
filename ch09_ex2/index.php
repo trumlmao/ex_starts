8c632e9209e2b58c67d4051e90353fb5d72273a1
Bùi Thế Dũng <trumeakao2001@gmail.com>
2023-11-13 16:59:46 +0700

    solved

--- ch09_ex2/index.php
+++ ch09_ex2/index.php
@@ -4,7 +4,7 @@ $number1 = 78;
 $number2 = -105.33;
 $number3 = 0.0049;
 $message = 'Enter some numbers and click on the Submit button.';
-
+$test = '';
 //process
 $action = filter_input(INPUT_POST, 'action');
 switch ($action) {
@@ -14,34 +14,41 @@ switch ($action) {
         $number3 = filter_input(INPUT_POST, 'number3');
 
         // make sure the user enters three numbers
-
+        if(!isset($number1)||!isset($number2)||!isset($number3)){
+            $test = 'false';
+        }
         // make sure the numbers are valid
-
+        if(!is_numeric($number1)||!is_numeric($number2)||!isset($number3)){
+            $test = 'false';
+        }
         // get the ceiling and floor for $number2
-
+        $number2_ceil = ceil($number2);
+        $number2_floor = floor($number3);
         // round $number3 to 3 decimal places
-
+        $roundedNumber3 = round($number3, 3);
         // get the max and min values of all three numbers
-
+        $max = max($number1,$number2,$number3);
+        $min = min($number1,$number2,$number3);
         // generate a random integer between 1 and 100
-
+        $random = rand(1,100);
         // format the message
-        $message = "This page is under construction.\n" .
-                   "Please write the code that processes the data.";
-        //$message =
-        //    "Number 1: $number1\n" .
-        //    "Number 2: $number2\n" .
-        //    "Number 3: $number3\n" .
-        //    "\n" .
-        //    "Number 2 ceiling: $number2_ceil\n" .
-        //    "Number 2 floor: $number2_floor\n" .
-        //    "Number 3 rounded: $number3_rounded\n" .
-        //    "\n" .
-        //    "Min: $min\n" .
-        //    "Max: $max\n" .
-        //    "\n" .
-        //    "Random: $random\n";
-
+        if ($test == 'false') {
+            $message = "PLS check info before submit";
+        } else {
+            $message =
+                "Number 1: $number1\n" .
+                "Number 2: $number2\n" .
+                "Number 3: $number3\n" .
+                "\n" .
+                "Number 2 ceiling: $number2_ceil\n" .
+                "Number 2 floor: $number2_floor\n" .
+                "Number 3 rounded: $roundedNumber3\n" .
+                "\n" .
+                "Min: $min\n" .
+                "Max: $max\n" .
+                "\n" .
+                "Random: $random\n";
+        }        
         break;
 }
 include 'number_tester.php';
