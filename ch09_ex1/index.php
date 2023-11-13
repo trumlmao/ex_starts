f93328aae0d25486cfe0c469353ac57460f544d6
Bùi Thế Dũng <trumeakao2001@gmail.com>
2023-11-13 16:40:19 +0700

    sloved

--- ch09_ex1/index.php
+++ ch09_ex1/index.php
@@ -13,17 +13,34 @@ switch ($action) {
         $name = filter_input(INPUT_POST, 'name');
         $email = filter_input(INPUT_POST, 'email');
         $phone = filter_input(INPUT_POST, 'phone');
+        $test = '';
 
         /*************************************************
          * validate and process the name
          ************************************************/
+        if (isset($name)===FALSE) {
+            $test ='false';
+            
+        }else{
+            $nameaf = ucwords($name);
+            $nameParts = explode(" ", $nameaf);
+            $firstName = $nameParts[0];
+            $firstName = ucfirst(strtolower($firstName));
+            
+        }
         // 1. make sure the user enters a name
         // 2. display the name with only the first letter capitalized
 
         /*************************************************
          * validate and process the email address
          ************************************************/
-        // 1. make sure the user enters an email
+        if (isset($email)) {
+            if (strpos($email, '@') === false || strpos($email, '.') === false) {
+                $test = 'false';
+                
+            }
+        }
+        
         // 2. make sure the email address has at least one @ sign and one dot character
 
         /*************************************************
@@ -31,12 +48,35 @@ switch ($action) {
          ************************************************/
         // 1. make sure the user enters at least seven digits, not including formatting characters
         // 2. format the phone number like this 123-4567 or this 123-456-7890
+        $phone = "1234567890"; // Replace with your actual phone number
+
+if (!is_numeric($phone) || strlen($phone) < 7 || strlen($phone) > 12) {
+    $test = 'false';
+    
+} else {
+    // Format the phone number
+    $phone = preg_replace("/^(\d{3})(\d{4})(\d{0,4})$/", "$1-$2-$3", $phone);
+
+    // Output the formatted phone number
+    
+}
 
         /*************************************************
          * Display the validation message
+         * $message = "Hello ".$firstName ."/n".
          ************************************************/
-        $message = "This page is under construction.\n" .
-                   "Please write the code that processes the data.";
+        if($test !== "false"){
+            $message = "Hello ".$firstName ."\n".
+                        "Thank you for entering this data:\n".
+                        "Name: ".$nameaf."\n".
+                        "Email: ".$email."\n".
+                        "Phone: ".$phone
+            
+            ;
+        }else{
+            $message ="PLS check info before submit";
+        }
+        
 
         break;
 }
