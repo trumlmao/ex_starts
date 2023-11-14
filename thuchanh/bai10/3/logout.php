9e17c83fac68b8e9173b4b9d552cd5d8d37ecb48
Bùi Thế Dũng <trumeakao2001@gmail.com>
2023-11-14 19:23:34 +0700

    solved

---
+++ thuchanh/bai10/3/logout.php
@@ -0,0 +1,8 @@
+<?php
+session_start();
+session_destroy();
+
+setcookie('username', '', time() - 1);
+setcookie('password', '', time() - 1);
+echo "<script>alert('Logout Successfull.'); location.href='index.php';</script>";
+?>
